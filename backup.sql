3c672a4e05062af325c407b2c98a986459aeca9c
henryguillen00 <henryguillen00@outlook.com>
2021-07-21 20:45:26 -0500

    Actualización base de datos

--- backup.sql
+++ backup.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-07-2021 a las 23:19:13
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 7.3.29
+-- Tiempo de generación: 22-07-2021 a las 03:44:25
+-- Versión del servidor: 8.0.25
+-- Versión de PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `direcciones` (
-  `usuario_id` int(11) NOT NULL,
+  `usuario_id` int NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `direccion` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `direcciones`
@@ -47,11 +47,11 @@ INSERT INTO `direcciones` (`usuario_id`, `nombre`, `direccion`) VALUES
 --
 
 CREATE TABLE `garantias` (
-  `id` int(11) NOT NULL,
-  `codigo` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `codigo` int NOT NULL,
   `problema` varchar(100) NOT NULL,
   `descripcion` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -60,26 +60,28 @@ CREATE TABLE `garantias` (
 --
 
 CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `descripcion` text NOT NULL,
-  `precio` int(11) NOT NULL,
-  `stock` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `categoria` varchar(50) NOT NULL,
+  `precio` int NOT NULL,
+  `stock` int NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `stock`) VALUES
-(1, 'PC Proaso 3000', 'Un computador construido en la nasa', 100000, 0),
-(2, 'PC Proaso 5000', 'Esto es una descripción del producto, aún es n00b.', 50000, 1),
-(3, 'Prueba', 'sdfsdfo;jshdflkjsdfhkjsdhfaskjfhsajkdfashdfkjashfkjsfhsjkfhkjsdfhjksdfhskjdfhskdjfhksjdfhskjfhsjkdfhsjkdfhskjdfhakdfjhasfkjahsfjkhsdkfhsakjdfhskjdfhskjdfhaskjdfhaskdfhadfkjahsdfkhskdjfhksjdfhksjdf', 11, 1),
-(4, 'Cristian Camilo', 'Hola mundo!', 6506540, 0),
-(5, 'Cristian Camilo', 'Hola mundo!', 6506540, 1),
-(6, 'Bedoya Time', 'Hola mundo!', 100000, 1),
-(7, 'Bedoya Time', 'Hola mundo!', 100000, 1),
-(8, 'RAM x64 - PC Master', '64 GB - Holaaa', 10000, 0);
+INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `categoria`, `precio`, `stock`) VALUES
+(1, 'PC Proaso 3000', 'Un computador construido en la nasa', 'computador', 100000, 0),
+(2, 'PC Proaso 5000', 'Esto es una descripción del producto, aún es n00b.', 'computador', 50000, 1),
+(3, 'Prueba', 'sdfsdfo;jshdflkjsdfhkjs', 'texto', 11, 1),
+(4, 'Cristian Camilo', 'Hola mundo!', 'texto', 6506540, 1),
+(5, 'Cristian Camilo', 'Hola mundo!', 'texto', 6506540, 1),
+(6, 'Bedoya Time', 'Hola mundo!', 'texto', 100000, 1),
+(7, 'Bedoya Time', 'Hola mundo!', 'texto', 100000, 1),
+(8, 'RAM x64 - PC Master', '64 GB - Holaaa', 'componente', 10000, 1),
+(9, 'Pc master race', 'super pc gg', 'computador', 20000, 2);
 
 -- --------------------------------------------------------
 
@@ -88,12 +90,12 @@ INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `stock`) VALUE
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
-  `administrador` int(11) NOT NULL DEFAULT 0,
+  `administrador` int NOT NULL DEFAULT '0',
   `contrasenia` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -109,11 +111,11 @@ INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `administrador`, `contrasenia`
 --
 
 CREATE TABLE `ventas` (
-  `id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `fecha_compra` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int NOT NULL,
+  `usuario_id` int NOT NULL,
+  `fecha_compra` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `direccion` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `ventas`
@@ -129,7 +131,8 @@ INSERT INTO `ventas` (`id`, `usuario_id`, `fecha_compra`, `direccion`) VALUES
 (7, 1, '2021-07-21 20:58:50', 'Cra 19 11N 33'),
 (8, 1, '2021-07-21 21:08:05', 'Cra 19 11N 33'),
 (9, 1, '2021-07-21 21:08:13', 'Cra 19 11N 33'),
-(10, 1, '2021-07-21 21:08:49', 'Cra 19 11N 33');
+(10, 1, '2021-07-21 21:08:49', 'Cra 19 11N 33'),
+(11, 1, '2021-07-21 22:22:25', 'Cra 19 11N 33');
 
 -- --------------------------------------------------------
 
@@ -138,10 +141,10 @@ INSERT INTO `ventas` (`id`, `usuario_id`, `fecha_compra`, `direccion`) VALUES
 --
 
 CREATE TABLE `ventas_producto` (
-  `venta_id` int(11) NOT NULL,
-  `producto_id` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `venta_id` int NOT NULL,
+  `producto_id` int NOT NULL,
+  `cantidad` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `ventas_producto`
@@ -155,7 +158,8 @@ INSERT INTO `ventas_producto` (`venta_id`, `producto_id`, `cantidad`) VALUES
 (5, 2, 1),
 (5, 4, 1),
 (6, 8, 1),
-(10, 2, 9);
+(10, 2, 9),
+(11, 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -201,25 +205,25 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `garantias`
 --
 ALTER TABLE `garantias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Restricciones para tablas volcadas
